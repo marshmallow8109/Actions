ad1f0b848843e760c978e91cfee7b9f155cfa940
Austine <augustrush.ae@gmail.com>
2025-02-11 19:24:19 +0100

    add

--- App.js
+++ App.js
@@ -3,3 +3,15 @@ console.log("hello world");
 console.log("just checking--- adding more code");
 console.log("just checking--- adding more code");
 console.log("just checking--- adding more code");
+
+const data = (name) => {
+  console.log(name);
+};
+
+data("hello");
+
+const getrandom = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+getrandom(7);
